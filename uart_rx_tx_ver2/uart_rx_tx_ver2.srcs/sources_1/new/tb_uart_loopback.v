396b95ea02b50cf09803a49398bb8f56825d6137
younghyun0702 <younghyun0702@gmail.com>
2026-04-22 21:13:56 +0900

    문자열 가능한 UAART 모듈 추가

--- uart_rx_tx_ver2/uart_rx_tx_ver2.srcs/sources_1/new/tb_uart_loopback.v
+++ uart_rx_tx_ver2/uart_rx_tx_ver2.srcs/sources_1/new/tb_uart_loopback.v
@@ -1,17 +1,14 @@
 `timescale 1ns / 1ps
 
-
-
 module tb_uart_loopback ();
 
-    parameter BAUD_PERIOD = (100_000_000 / 9600) * 10;
-
+    parameter BAUD_PERIOD = (100_000_000 / 9600) * 10;  //한클럭 10n
     reg [7:0] compare_data;
 
     reg clk, rst, rx;
     wire tx;
 
-    uart_loopback U0 (
+    uart_loopback dut (
         .clk(clk),
         .rst(rst),
         .rx (rx),
@@ -22,45 +19,45 @@ module tb_uart_loopback ();
 
     integer i;
 
-    task SENDER_UART(input [7:0] send_data);
-        begin  // pc tx
+    task SENDER_UART(
+        input [7:0] send_data
+    );  // 시간값을 만들어 넣을 수 있음
+        begin
+            // pc tx
             // start
             rx = 0;
-
-            //START BIT
+            // start bit
             #(BAUD_PERIOD);
-
+            // data bit
             for (i = 0; i < 8; i = i + 1) begin
-                // rx, send data[0] ~ [7]
+                // rx, send_data[0] ~ [7]
                 rx = send_data[i];
                 #(BAUD_PERIOD);
             end
-
+            // stop
             rx = 1;
             #(BAUD_PERIOD);
-
         end
     endtask
-
 
     initial begin
         clk = 0;
         rst = 1;
-        rx = 1;
-        compare_data = 8'h30;
+        rx = 1;  // 초기값 start 1
+        compare_data = 8'h30;  // ascii '0'
         @(negedge clk);
         @(negedge clk);
-
         rst = 0;
 
+        repeat (10000) @(negedge clk);
+
         SENDER_UART(compare_data);
-        #(BAUD_PERIOD * 10);
-        SENDER_UART(compare_data);
+
         #(BAUD_PERIOD * 10);
         #1000;
         $stop;
 
-
     end
+
 
 endmodule
